a7a9eee9b0210b97558da9fdc1481092d6ce1d00
Julien Garagnon <jupotter.g@gmail.com>
2015-08-21 11:47:23 +0200

    add robots.txt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,22 @@ module.exports = function(grunt) {
                     "dist/sierra.html": "static/sierra.html",
                     "dist/zombie.html": "static/zombie.html",
                     "dist/light-out.html": "static/light-out.html",
-                }
+                    "dist/robots.txt": "static/robots.txt",
+                },
             },
         },
+        copy: {
+            files: {
+                cwd: "static/content",
+                src: "**/*",
+                dest: "dist/content",
+                expand: true,
+            },
+        }
     } )
 
     grunt.loadNpmTasks( "grunt-bake" );
+    grunt.loadNpmTasks( "grunt-copy" );
 
-    grunt.registerTask('default', ['bake']);
+    grunt.registerTask('default', ['bake', 'copy']);
 };
